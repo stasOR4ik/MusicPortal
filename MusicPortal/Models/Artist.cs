f787078ed82a512feb7820bacdc5f49a1b00d2cc
stasOR4ik <kov-stas99@mail.ru>
2018-06-05 11:55:17 +0300

    Adding classes(Artist, Album, Track)

---
+++ MusicPortal/Models/Artist.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPortal.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private string PictureLink { get; set; }
+        private string Biography { get; set; }
+        public List<Track> Tracks { get; set; }
+        public List<Album> Albums { get; set; }
+        public List<string> Tags { get; set; }
+
+        public Artist() { }
+
+        public Artist(string name)
+        {
+            Name = name;
+            Tracks = new List<Track>();
+            Albums = new List<Album>();
+            Tags = new List<string>();
+        }
+
+        public void SetPictureLink(string picturelink)
+        {
+            string defultPictureLink = "https://lastfm-img2.akamaized.net/i/u/avatar170s/2a96cbd8b46e442fc41c2b86b821562f";
+            PictureLink = picturelink == "" ? defultPictureLink : picturelink;
+        }
+
+        public string GetPictureLink()
+        {
+            return PictureLink;
+        }
+
+        public void SetBiography(string biography)
+        {
+            Biography = biography;
+        }
+
+        public string GetBiography()
+        {
+            return Biography;
+        }
+    }
+}
---
+++ MusicPortal/Models/Track.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPortal.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private string PictureLink { get; set; }
+        private string Duration { get; set; }
+        public List<string> Tags { get; set; }
+
+        public Track() { }
+
+        public Track(string name)
+        {
+            Name = name;
+            Tags = new List<string>();
+        }
+
+        public void SetPictureLink(string pictureLink)
+        {
+            string defaultPictureLink = "https://lastfm-img2.akamaized.net/i/u/174s/4128a6eb29f94943c9d206c08e625904";
+            PictureLink = pictureLink == "" ? defaultPictureLink : pictureLink;
+        }
+
+        public string GetPictureLink()
+        {
+            return PictureLink;
+        }
+
+        public void SetDurationInMilliseconds(string millisecondsDuration)
+        {
+            if (millisecondsDuration.Length <= 3)
+            {
+                Duration = "0:00";
+            }
+            else
+            {
+                SetDuration(millisecondsDuration.Substring(0, millisecondsDuration.Length - 3));
+            }
+        }
+
+        public void SetDuration(string secondsDuration)
+        {
+            int seconds = Convert.ToInt32(secondsDuration);
+            int minutes = seconds / 60;
+            seconds %= 60;
+            Duration = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+        }
+
+        public string GetDuration()
+        {
+            return Duration;
+        }
+
+    }
+}
