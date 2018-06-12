4e95cb8dfa4fde0607b2d1b4489b86cd6c29427f
stasOR4ik <kov-stas99@mail.ru>
2018-06-12 14:16:41 +0300

    Adding pagination

---
+++ MusicPortal/Models/Index.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPortal.Models
+{
+    public class Index
+    {
+        public List<Artist> Artists { get; set; }
+        public int Page { get; set; }
+        public int NumberOfArtistsOnStartPage { get; set; }
+
+        public Index() { }
+
+        public Index(List<Artist> artists, int page, int namberOfArtistsOnStartPage)
+        {
+            Artists = artists;
+            Page = page;
+            NumberOfArtistsOnStartPage = namberOfArtistsOnStartPage;
+        }
+    }
+}
