66c85b0126f19587539c35d5a94e5353952a4408
stasOR4ik <kov-stas99@mail.ru>
2018-06-21 11:35:01 +0300

    Adding pattern Repository

---
+++ Repo/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    interface IRepository<T>
+        where T : class
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAllBy(Expression<Func<T, bool>> predicate);
+        T GetBy(Expression<Func<T, bool>> predicate); 
+        void Create(T item);
+        void Update(T item); 
+        void Delete(Expression<Func<T, bool>> predicate);
+        void Save();
+    }
+}
---
+++ Repo/MusicRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    class MusicRepository<T> : IRepository<T>
+        where T : class
+    {
+        MusicContext _context;
+        DbSet<T> _db;
+
+        public MusicRepository(MusicContext context)
+        {
+            _context = context;
+            _db = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _db;
+        }
+
+        public IEnumerable<T> GetAllBy(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Where(predicate);
+        }
+
+        public void Create(T item)
+        {
+            _db.Add(item);
+        }
+
+        public void Delete(Expression<Func<T, bool>> predicate)
+        {
+            T item = _db.Find(predicate);
+            if (item != null)
+                _db.Remove(item);
+        }
+
+        public T GetBy(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Find(predicate);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            _db.Update(item);
+        }
+    }
+}
+}
